45515dc4d8a58602e41f152ff1f7f33da36a2224
Scalf99 <siegjr00@gmail.com>
2025-05-21 18:03:37 +0100

    e

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = 'https://vjryqofxliygwmdjjgcm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqcnlxb2Z4bGl5Z3dtZGpqZ2NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4NDU5MzAsImV4cCI6MjA2MzQyMTkzMH0.nmOr5PnN2SeDUzbCVgDebhoankYXYulmzFL_VtBL3W8';
 
 // Initialize Supabase client
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // Export Supabase client
-window.SupabaseDB = supabase; 
+window.SupabaseDB = supabaseClient; 
